171d3a4681860a2bdec7dfd8ae0239c890be0fe5
p-reetu <79264123+p-reetu@users.noreply.github.com>
2024-06-16 19:01:25 +0530

    Added card component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,15 @@
+import React from 'react';
+import "./Main.css"
+import Card from './Card';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
 export default function Main(props) {
-    return(
-        <div></div>
+    return (
+        <Container className="card-container">
+            {props.gifts.map(e => <Card gift={e}/> )}
+        </Container>
     )
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,23 @@
-import "./Nav.css"
+import React from 'react';
+import "./Nav.css";
+import logo from '../assets/logo.png'; 
+import profileIcon from '../assets/profileIcon.png'; 
 
-export default function Nav(){
+export default function Nav() {
     return (
-        <div className="navbar">This is navbar</div>
-    )
+        <nav className="navbar">
+            <div className="nav-left">
+                <img src={logo} alt="Logo" className="nav-logo" />
+            </div>
+            <div className="nav-center">
+                <div className="nav-combined-button">
+                    <button className="nav-button-left">Gifts Received</button>
+                    <button className="nav-button-right">Gifts Sent</button>
+                </div>
+            </div>
+            <div className="nav-right">
+                <img src={profileIcon} alt="Profile" className="profile-icon" />
+            </div>
+        </nav>
+    );
 }
