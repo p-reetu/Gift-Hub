123fe6082b8cd97bdf2c2eeb4e2552ff5574e04f
p-reetu <79264123+p-reetu@users.noreply.github.com>
2024-06-18 10:27:27 +0530

    Added pages and Login component

--- src/App.js
+++ src/App.js
@@ -1,47 +1,60 @@
+import { BrowserRouter, Link, Route, Router, Routes } from "react-router-dom";
 import "./App.css";
-import Main from "./components/Main";
-import Nav from "./components/Nav";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import ErrorPage from "./pages/ErrorPage";
 
 function App() {
   const gifts = [
     {
       date: "1 June, 2024",
-      image: "https://github.com/p-reetu/Gift-Hub/blob/master/src/assets/bracelet.jpg?raw=true",
+      image:
+        "https://github.com/p-reetu/Gift-Hub/blob/master/src/assets/bracelet.jpg?raw=true",
       name: "Bracelet",
       description:
         "Received this special gift from my best friend on my birthday. I loved it a lot. One of the most beautiful gifts.",
     },
     {
       date: "1 June, 2024",
-      image: "https://github.com/p-reetu/Gift-Hub/blob/master/src/assets/bracelet.jpg?raw=true",
+      image:
+        "https://github.com/p-reetu/Gift-Hub/blob/master/src/assets/bracelet.jpg?raw=true",
       name: "Bracelet",
       description:
         "Received this special gift from my best friend on my birthday. I loved it a lot. One of the most beautiful gifts.",
     },
     {
       date: "1 June, 2024",
-      image: "https://github.com/p-reetu/Gift-Hub/blob/master/src/assets/bracelet.jpg?raw=true",
+      image:
+        "https://github.com/p-reetu/Gift-Hub/blob/master/src/assets/bracelet.jpg?raw=true",
       name: "Bracelet",
-      description: "Received this special gift from my best friend on my birthday. I loved it a lot. One of the most beautiful gifts."
+      description:
+        "Received this special gift from my best friend on my birthday. I loved it a lot. One of the most beautiful gifts.",
     },
     {
       date: "1 June, 2024",
-      image: "https://github.com/p-reetu/Gift-Hub/blob/master/src/assets/bracelet.jpg?raw=true",
+      image:
+        "https://github.com/p-reetu/Gift-Hub/blob/master/src/assets/bracelet.jpg?raw=true",
       name: "Bracelet",
-      description: "Received this special gift from my best friend on my birthday. I loved it a lot. One of the most beautiful gifts."
+      description:
+        "Received this special gift from my best friend on my birthday. I loved it a lot. One of the most beautiful gifts.",
     },
     {
       date: "1 June, 2024",
-      image: "https://github.com/p-reetu/Gift-Hub/blob/master/src/assets/bracelet.jpg?raw=true",
+      image:
+        "https://github.com/p-reetu/Gift-Hub/blob/master/src/assets/bracelet.jpg?raw=true",
       name: "Bracelet",
-      description: "Received this special gift from my best friend on my birthday. I loved it a lot. One of the most beautiful gifts."
-    }
+      description:
+        "Received this special gift from my best friend on my birthday. I loved it a lot. One of the most beautiful gifts.",
+    },
   ];
   return (
-    <div className="App">
-      <Nav />
-      <Main gifts={gifts} />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage gifts={gifts} />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="*" element={<ErrorPage />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,8 +2,8 @@ import React from 'react';
 import "./Main.css"
 import Card from './Card';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+//import Row from 'react-bootstrap/Row';
+//import Col from 'react-bootstrap/Col';
 
 
 export default function Main(props) {
